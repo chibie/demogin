ec77f84901b37a7e0c1b115ac0d1c64adcc221a2
Chibuotu Amadi <chibuotu@gmail.com>
2022-04-26 03:04:11 +0100

    patch book

--- controllers/books.go
+++ controllers/books.go
@@ -7,9 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Input types
 type CreateBookInput struct {
 	Title  string `json:"title" binding:"required"`
 	Author string `json:"author" binding:"required"`
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
 // POST /books
@@ -38,6 +44,28 @@ func FindBook(c *gin.Context) { // Get model if exist
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+// PATCH /books/:id
+// Update a book
+func UpdateBook(c *gin.Context) {
+	// Get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	// Validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
